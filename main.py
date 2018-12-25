8bf434e6b59161fe0c4335c51fbad2756b677d62
alimq <alimimanmalik2@gmail.com>
2018-12-26 01:26:21 +0600

    no message

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class PaintApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle('Paint')
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    paint = PaintApp()
+    paint.showMaximized()
+    sys.exit(app.exec())
