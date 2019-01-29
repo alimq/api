536cc0ba2d06f1294c3010b9060c86b5c39ca0f2
alimq <alimimanmalik2@gmail.com>
2019-01-29 20:53:39 +0600

    commit

--- canvas.py
+++ canvas.py
@@ -6,25 +6,31 @@ from PyQt5.QtWidgets import *
 class Canvas(QLabel):
     def __init__(self, parent=None):
         super().__init__(parent)
-        p = self.palette()
-        p.setColor(self.backgroundRole(), Qt.white)
+        palette = self.palette()
+        palette.setColor(self.backgroundRole(), Qt.white)
         self.setAutoFillBackground(True)
-        self.setPalette(p)
-        self.myPixmap = QPixmap(600, 600)
-        self.painter = QPainter(self.myPixmap)
-        self.pen = QPen()
-        self.painter.setPen(self.pen)
+        self.setPalette(palette)
+        self.currentPixmap = QPixmap(600, 600)
+        self.painter = QPainter(self.currentPixmap)
         self.painter.fillRect(0, 0, 600, 600, Qt.white)
-        self.setPixmap(self.myPixmap)
-        self.last = None    
+        self.setPixmap(self.currentPixmap)
+        self.pen = QPen()
+        self.last = None   
+        
+        self.eraser = QPen()
+        self.eraser.setColor(Qt.white)
+        self.eraser.setJoinStyle(Qt.RoundJoin)
+        self.eraser.setCapStyle(Qt.RoundCap)
+        
+        self.currentPen = self.pen
 
     def mouseMoveEvent(self, event):
         if self.last:
-            self.painter.setPen(self.pen)
+            self.painter.setPen(self.currentPen)
             self.painter.drawLine(self.last, event.pos())
             
             self.last = event.pos()
-            self.setPixmap(self.myPixmap)
+            self.setPixmap(self.currentPixmap)
             self.update()
 
     def mousePressEvent(self, event):
@@ -32,4 +38,3 @@ class Canvas(QLabel):
         
     def mouseReleaseEvent(self, event):
         self.last = None
-
--- main.py
+++ main.py
@@ -12,48 +12,71 @@ class App(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.brushSizeSlider.setMinimum(1)
-        self.brushSizeSlider.setMaximum(24)
-        self.brushSizeSlider.setTickInterval(1)
-        self.brushSizeSlider.valueChanged.connect(self.changeBrushSize)
-        self.brushColorButton.clicked.connect(self.changeBrushColor)
+        self.penSizeSlider.setMinimum(1)
+        self.penSizeSlider.setMaximum(24)
+        self.penSizeSlider.setTickInterval(1)
+        self.penSizeSlider.valueChanged.connect(self.changePenSize)
+        
+        self.penColorButton.clicked.connect(self.changePenColor)
         self.clearButton.clicked.connect(self.clearCanvas)
         self.actionSaveAs.triggered.connect(self.saveAs)
         self.actionOpenFile.triggered.connect(self.openFile)
-        self.brushHardSoft.clicked.connect(self.changeHardSoft)
+        self.penHardSoft.clicked.connect(self.changeHardSoft)
+        self.penEraserButton.clicked.connect(self.changePenEraser)
         
-    def changeBrushSize(self):
-        self.canvas.pen.setWidth(self.brushSizeSlider.value())
+        self.eraserSizeSlider.setMinimum(2)
+        self.eraserSizeSlider.setMaximum(48)
+        self.eraserSizeSlider.setTickInterval(2)
+        self.eraserSizeSlider.valueChanged.connect(self.changeEraserSize)        
         
-    def changeBrushColor(self):
+        
+    def changePenSize(self):
+        self.canvas.pen.setWidth(self.penSizeSlider.value())
+        newLabel = 'Current pen size(' + str(self.penSizeSlider.value()) + '):'
+        self.penSizeLabel.setText(newLabel)
+        
+    def changePenColor(self):
         color = QColorDialog().getColor()
         self.canvas.pen.setColor(color)
         
     def clearCanvas(self):
         self.canvas.painter.fillRect(0, 0, 600, 600, Qt.white)
-        self.canvas.setPixmap(self.canvas.myPixmap)
+        self.canvas.setPixmap(self.canvas.currentPixmap)
         
     def saveAs(self):
         saveAs_file = QFileDialog.getSaveFileName(None, 'Save File:', 'untitled.png', 'Images (*.png *.bmp *.jpg)')
-        saveAs_ok = self.canvas.myPixmap.save(saveAs_file[0])
+        saveAs_ok = self.canvas.currentPixmap.save(saveAs_file[0])
         print('save', saveAs_ok)
         
     def openFile(self):
         openFile_file = QFileDialog.getOpenFileName(None, 'Open File:', '', 'Images (*.png *.bmp *.jpg)')
-        openFile_ok = self.canvas.myPixmap.load(openFile_file[0])
-        self.canvas.setPixmap(self.canvas.myPixmap)
+        openFile_ok = self.canvas.currentPixmap.load(openFile_file[0])
+        self.canvas.setPixmap(self.canvas.currentPixmap)
         self.canvas.update()
         print('open', openFile_ok)
         
     def changeHardSoft(self):
-        if self.brushHardSoft.text() == 'Hard':
+        if self.penHardSoft.text() == 'Hard':
             self.canvas.pen.setCapStyle(Qt.RoundCap)
             self.canvas.pen.setJoinStyle(Qt.RoundJoin)
-            self.brushHardSoft.setText('Soft')
-        elif self.brushHardSoft.text() == 'Soft':
+            self.penHardSoft.setText('Soft')
+        elif self.penHardSoft.text() == 'Soft':
             self.canvas.pen.setCapStyle(Qt.SquareCap)
             self.canvas.pen.setJoinStyle(Qt.BevelJoin)
-            self.brushHardSoft.setText('Hard')
+            self.penHardSoft.setText('Hard')
+            
+    def changePenEraser(self):
+        if self.penEraserButton.text() == 'Pen':
+            self.canvas.currentPen = self.canvas.eraser
+            self.penEraserButton.setText('Eraser')
+        elif self.penEraserButton.text() == 'Eraser':
+            self.canvas.currentPen = self.canvas.pen
+            self.penEraserButton.setText('Pen')
+            
+    def changeEraserSize(self):
+        self.canvas.eraser.setWidth(self.eraserSizeSlider.value())
+        newLabel = 'Current eraser size(' + str(self.eraserSizeSlider.value()) + '):'
+        self.eraserSizeLabel.setText(newLabel)        
         
 
 if __name__ == '__main__':
