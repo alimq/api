adb42bace67ef1044fa1229ac79e931a9c72c97c
alimq <alimimanmalik2@gmail.com>
2019-04-25 00:56:15 +0600

    no message

--- main.py
+++ main.py
@@ -1,129 +1,301 @@
-# -*- coding: utf-8 -*-
-import sys
-from PyQt5 import uic
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from ui import Ui_MainWindow
+import pygame
+import os
+import random
 
+def load_image(name, colorkey=None):
+    fullname = os.path.join('', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
 
-class App(QMainWindow, Ui_MainWindow):
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+running = True
+pygame.init()
+size = w, h = 800, 600
+screen = pygame.display.set_mode(size)
+keys = {32: 'jump',
+        274: 'down',
+        275: 'right',
+        276: 'left'}
+
+user_score = 0
+all_sprites = pygame.sprite.Group()
+obstacle_sprites = pygame.sprite.Group()
+vertical_sprites = pygame.sprite.Group()
+horizontal_sprites = pygame.sprite.Group()
+hero_sprites = pygame.sprite.Group()
+bomb_sprites = pygame.sprite.Group()
+drop_sprites = pygame.sprite.Group()
+trap_sprites = pygame.sprite.Group()
+distance = -370
+
+all_obstacles = []
+all_verticals = []
+all_horizontals = []
+all_drops = []
+all_bombs = []
+all_traps = []
+
+def update_user_score():
+    global user_score
+    font = pygame.font.Font(None, 22)
+    label = 'Score: ' + str(user_score)
+    text = font.render(label, 1, pygame.Color('darkgreen'))
+    screen.blit(text, (25, 30))
+    
+def user_lost():
+    global running
+    screen.fill(pygame.Color('white'))
+    font = pygame.font.Font(None, 50)
+    label = 'You lost! Score: ' + str(user_score)
+    text = font.render(label, 1, pygame.Color('red'))
+    screen.blit(text, (250, 250))    
+    pygame.display.flip()
+    running = False
+    pygame.time.delay(1500)
+
+class Vertical:
+    def __init__(self, x, y):
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = load_image('vertical.png')
+        self.sprite.rect = self.sprite.image.get_rect()
+        all_sprites.add(self.sprite)
+        vertical_sprites.add(self.sprite)
+        self.x = x
+        self.y = y
+        self.sprite.rect.x = x
+        self.sprite.rect.y = y 
+        
+    def move(self):
+        self.sprite.rect.x = self.x - distance   
+        
+class Horizontal:
+    def __init__(self, x, y):
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = load_image('horizontal.png')
+        self.sprite.rect = self.sprite.image.get_rect()
+        all_sprites.add(self.sprite)
+        horizontal_sprites.add(self.sprite)
+        self.x = x
+        self.y = y
+        self.sprite.rect.x = x
+        self.sprite.rect.y = y 
+        
+    def move(self):
+        self.sprite.rect.x = self.x - distance   
+
+class Obstacle:
+    def __init__(self, x, y):
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = load_image('obstacle.png')
+        self.sprite.rect = self.sprite.image.get_rect()
+        all_sprites.add(self.sprite)
+        obstacle_sprites.add(self.sprite)
+        self.x = x
+        self.y = y
+        self.sprite.rect.x = x
+        self.sprite.rect.y = y
+        all_verticals.append(Vertical(self.x - 4, self.y + 5))
+        all_verticals.append(Vertical(self.x + 101, self.y + 5))
+        all_horizontals.append(Horizontal(self.x, self.y + 80))        
+        
+    def move(self):
+        self.sprite.rect.x = self.x - distance 
+        
+class Trap:
+    def __init__(self, x, y):
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = load_image('trap.png')
+        self.sprite.rect = self.sprite.image.get_rect()
+        all_sprites.add(self.sprite)
+        trap_sprites.add(self.sprite)
+        self.x = x
+        self.y = y
+        self.sprite.rect.x = x
+        self.sprite.rect.y = y
+        all_horizontals.append(Horizontal(self.x, self.y + 80))        
+        
+    def move(self):
+        self.sprite.rect.x = self.x - distance     
+
+def update_obstacles():
+    for obstacle in all_obstacles:
+        obstacle.move()
+    for vertical in all_verticals:
+        vertical.move()  
+    for horizontal in all_horizontals:
+        horizontal.move()
+    for drop in all_drops:
+        drop.move()
+    for bomb in all_bombs:
+        bomb.move()    
+    for trap in all_traps:
+        trap.move()
+
+class Hero:
+    def __init__(self, src):
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = load_image(src)
+        self.sprite.rect = self.sprite.image.get_rect()
+        all_sprites.add(self.sprite)
+        hero_sprites.add(self.sprite)
+        self.sprite.rect.x = 370
+        self.sprite.rect.y = 520
+        self.jump = False
+        self.right = False
+        self.left = False
+        self.fall = False
+        self.jumping_order = [-15, -14, -13, -12, -10, -8, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 8, 10, 12, 13, 14, 15]
+        self.jumping_index = 0
+        
+    def on_keydown(self, key):
+        direction = keys.get(key, '')
+        if direction == 'jump':
+            self.jump = True
+        elif direction == 'right':
+            self.right = True
+        elif direction == 'left':
+            self.left = True
+            
+    def on_keyup(self, key):
+        direction = keys.get(key, '')
+        if direction == 'right':
+            self.right = False
+        elif direction == 'left':
+            self.left = False
+            
+    def move(self):
+        if pygame.sprite.spritecollideany(self.sprite, trap_sprites):
+            user_lost()
+            return
+        
+        if self.sprite.rect.y > 520:
+            self.sprite.rect.y = 520
+            self.fall = False
+            self.jump = False
+        if self.jump == True:
+            if self.jumping_index >= len(self.jumping_order):
+                self.jumping_index = 0
+                self.jump = False
+                return
+            self.sprite.rect.y += self.jumping_order[self.jumping_index]
+            self.jumping_index += 1
+            if pygame.sprite.spritecollideany(self.sprite, horizontal_sprites):
+                self.jumping_index -= 1
+                self.sprite.rect.y -= self.jumping_order[self.jumping_index]
+                self.jump = False                    
+            if pygame.sprite.spritecollideany(self.sprite, obstacle_sprites):
+                self.jumping_index = 0
+                self.jump = False        
+        global distance
+        if self.right == True:
+            distance += 4
+            update_obstacles()          
+            if pygame.sprite.spritecollideany(self.sprite, vertical_sprites):
+                distance -= 4        
+        if self.left == True:
+            distance -= 4   
+            update_obstacles()            
+            if pygame.sprite.spritecollideany(self.sprite, vertical_sprites):
+                distance += 4           
+        if self.jump or pygame.sprite.spritecollideany(self.sprite, obstacle_sprites) or self.sprite.rect.y >= 520:
+            self.fall = False
+        else:
+            self.fall = True
+        if self.fall:
+            self.sprite.rect.y += 8
+            
+class Drop:
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle('YandexLyceum Paint Project')
-        self.penSizeSlider.setMinimum(1)
-        self.penSizeSlider.setMaximum(24)
-        self.penSizeSlider.setTickInterval(1)
-        self.penSizeSlider.valueChanged.connect(self.changePenSize)
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = load_image('drop.png')
+        self.sprite.rect = self.sprite.image.get_rect()
+        all_sprites.add(self.sprite)
+        drop_sprites.add(self.sprite)
+        self.x = random.randint(distance, distance + 800)
+        self.y = 0
+        self.sprite.rect.x = self.x
+        self.sprite.rect.y = self.y    
+        self.dropped = False
         
-        self.currentFile = None
-        
-        self.penColorButton.clicked.connect(self.changePenColor)
-        self.clearButton.clicked.connect(self.clearCanvas)
-        self.actionSaveAs.triggered.connect(self.saveAs)
-        self.penHardSoft.clicked.connect(self.changeHardSoft)
-        self.penEraserButton.clicked.connect(self.changePenEraser)
-        
-        self.eraserSizeSlider.setMinimum(2)
-        self.eraserSizeSlider.setMaximum(48)
-        self.eraserSizeSlider.setTickInterval(2)
-        self.eraserSizeSlider.valueChanged.connect(self.changeEraserSize)     
-        
-        self.penTypeCombo.currentTextChanged.connect(self.changePenType)
-        self.actionSave.triggered.connect(self.saveFile)
-        
-        
-    def changePenSize(self):
-        self.canvas.pen.setWidth(self.penSizeSlider.value())
-        newLabel = 'Current pen size(' + str(self.penSizeSlider.value()) + '):'
-        self.penSizeLabel.setText(newLabel)
-        
-    def changePenColor(self):
-        color = QColorDialog().getColor()
-        self.canvas.pen.setColor(color)
-        self.canvas.brush.setColor(color)
-        
-    def clearCanvas(self):
-        self.canvas.painter.fillRect(0, 0, 600, 600, Qt.white)
-        self.canvas.setPixmap(self.canvas.currentPixmap)
-        
-    def saveAs(self):
-        saveAs_file = QFileDialog.getSaveFileName(None, 'Save File:', 'yandexlyceum.png', 'Images (*.png *.bmp *.jpg)')
-        saveAs_ok = self.canvas.currentPixmap.save(saveAs_file[0])
-        if saveAs_ok:
-            self.currentFile = saveAs_file[0]
-            print('Successfully saved into', self.currentFile)
-        else:
-            print('Fail!')
-        
-    def saveFile(self):
-        if self.currentFile:
-            saveAs_ok = self.canvas.currentPixmap.save(self.currentFile)
-            if saveAs_ok:
-                print('Successfully saved', self.currentFile)
-            else:
-                print('Fail!')
-        else:
-            self.saveAs()
-        
-    def changeHardSoft(self):
-        if self.penHardSoft.text() == 'Hard':
-            self.canvas.pen.setCapStyle(Qt.RoundCap)
-            self.canvas.pen.setJoinStyle(Qt.RoundJoin)
-            self.penHardSoft.setText('Soft')
-        elif self.penHardSoft.text() == 'Soft':
-            self.canvas.pen.setCapStyle(Qt.SquareCap)
-            self.canvas.pen.setJoinStyle(Qt.BevelJoin)
-            self.penHardSoft.setText('Hard')
+    def move(self):
+        global user_score
+        self.sprite.rect.y += random.randint(1, 5)
+        if (self.sprite.rect.y > 600 or pygame.sprite.spritecollideany(self.sprite, obstacle_sprites)):
+            self.sprite.kill()
+            self.dropped = True
+        if pygame.sprite.spritecollideany(self.sprite, hero_sprites) and not self.dropped:
+            self.sprite.kill()
+            self.dropped = True
             
-    def changePenEraser(self):
-        if self.penEraserButton.text() == 'Pen':
-            self.canvas.currentPen = self.canvas.eraser
-            self.penEraserButton.setText('Eraser')
-        elif self.penEraserButton.text() == 'Eraser':
-            self.canvas.currentPen = self.canvas.pen
-            self.penEraserButton.setText('Pen')
-            
-    def changeEraserSize(self):
-        self.canvas.eraser.setWidth(self.eraserSizeSlider.value())
-        newLabel = 'Current eraser size(' + str(self.eraserSizeSlider.value()) + '):'
-        self.eraserSizeLabel.setText(newLabel)        
+            user_score += 100
+            update_user_score()
+        self.sprite.rect.x = self.x - distance 
         
-    def changePenType(self):
-        val = self.penTypeCombo.currentText()
-        if val == 'Default':
-            self.canvas.brush.setStyle(Qt.SolidPattern)
-        elif val == 'Dense 1':
-            self.canvas.brush.setStyle(Qt.Dense1Pattern)
-        elif val == 'Dense 2':
-            self.canvas.brush.setStyle(Qt.Dense2Pattern)
-        elif val == 'Dense 3':
-            self.canvas.brush.setStyle(Qt.Dense3Pattern)
-        elif val == 'Dense 4':
-            self.canvas.brush.setStyle(Qt.Dense4Pattern)
-        elif val == 'Dense 5':
-            self.canvas.brush.setStyle(Qt.Dense5Pattern)
-        elif val == 'Dense 6':
-            self.canvas.brush.setStyle(Qt.Dense6Pattern)
-        elif val == 'Dense 7':
-            self.canvas.brush.setStyle(Qt.Dense7Pattern)       
-        elif val == 'Horizontal':
-            self.canvas.brush.setStyle(Qt.HorPattern)
-        elif val == 'Vertical':
-            self.canvas.brush.setStyle(Qt.VerPattern)
-        elif val == 'Cross':
-            self.canvas.brush.setStyle(Qt.CrossPattern)
-        elif val == 'BDiagonal':
-            self.canvas.brush.setStyle(Qt.BDiagPattern)
-        elif val == 'FDiagonal':
-            self.canvas.brush.setStyle(Qt.FDiagPattern)         
-        elif val == 'DiagonalCross':
-            self.canvas.brush.setStyle(Qt.DiagCrossPattern)    
+        
+class Bomb:
+    def __init__(self):
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = load_image('bomb.png')
+        self.sprite.rect = self.sprite.image.get_rect()
+        all_sprites.add(self.sprite)
+        bomb_sprites.add(self.sprite)
+        self.x = random.randint(distance, distance + 800)
+        self.y = 0
+        self.sprite.rect.x = self.x
+        self.sprite.rect.y = self.y    
+        self.dropped = False
+        
+    def move(self):
+        self.sprite.rect.y += random.randint(2, 7)
+        if (self.sprite.rect.y > 600 or pygame.sprite.spritecollideany(self.sprite, obstacle_sprites)):
+            self.sprite.kill()
+            self.dropped = True
+        if pygame.sprite.spritecollideany(self.sprite, hero_sprites) and not self.dropped:
+            self.sprite.kill()
+            self.dropped = True
+            user_lost()
+            return
+        self.sprite.rect.x = self.x - distance 
         
 
-if __name__ == '__main__':
-    application = QApplication(sys.argv)
-    example = App()
-    example.show()
-    sys.exit(application.exec_())
+fps = 60
+clock = pygame.time.Clock()
+hero = Hero('hero.png')
+all_obstacles.append(Obstacle(100, 520))
+all_obstacles.append(Obstacle(150, 440))
+all_obstacles.append(Obstacle(200, 360))
+all_obstacles.append(Obstacle(250, 280))
+all_obstacles.append(Obstacle(400, 520))
+all_traps.append(Trap(500, 570))
+all_obstacles.append(Obstacle(1000, 520))
+
+background = load_image('background.jpg')
+
+while running:
+    screen.blit(background, (0, 0))
+    update_user_score()
+    hero.move()
+    if random.randint(1, 240) == 1:
+        all_drops.append(Drop())
+    if random.randint(1, 150) == 1:
+        all_bombs.append(Bomb())    
+    update_obstacles()  
+    all_sprites.draw(screen)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            hero.on_keydown(event.key)
+        if event.type == pygame.KEYUP:
+            hero.on_keyup(event.key)        
+    pygame.display.flip()
+    clock.tick(fps)
